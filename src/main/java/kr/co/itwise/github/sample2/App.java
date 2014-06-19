499d8a1754f7d856656110e8329d3f51ea30c5dc
itwise-kkuru <kkuru@naver.com>
2014-06-19 19:16:53 +0900

    commit local repository

---
+++ src/main/java/kr/co/itwise/github/sample2/App.java
@@ -0,0 +1,13 @@
+package kr.co.itwise.github.sample2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
